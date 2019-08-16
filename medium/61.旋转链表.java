07d21da9495e03dafcc85385b275bda01efe8d76
F.LuVx <yeyu0926@hotmail.com>
2019-08-16 09:54:55 +0800

    61,206

--- medium/61.旋转链表.java
+++ medium/61.旋转链表.java
@@ -13,29 +13,52 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null || head.next == null || k == 0) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+        ListNode fakeHead = new ListNode(0);
+        fakeHead.next = head;
+        ListNode fast = fakeHead, slow = fakeHead;
+
+        int i;
+        for (i = 0; fast.next != null; i++) {
+            fast = fast.next;
+        }
+        for (int j = i - k % i; j > 0; j--) {
+            slow = slow.next;
+        }
+
+        fast.next = fakeHead.next;
+        fakeHead.next = slow.next;
+        slow.next = null;
+
+        return fakeHead.next;
+    }
+
+    public ListNode rotateRight0(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
             return head;
         }
         ListNode current = head, tail = null;
         int count = 0;
-        while(true) {
+        while (true) {
             count++;
-            if(current.next == null) {
+            if (current.next == null) {
                 tail = current;
                 tail.next = head;
                 break;
             }
             current = current.next;
         }
-        if(k == count) {
+        if (k == count) {
             tail.next = null;
             return head;
         }
         k = k < count ? k : k % count;
         count = count - k;
         current = head;
-        while(true) {
-            if(count == 1) {
+        while (true) {
+            if (count == 1) {
                 ListNode next = current.next;
                 current.next = null;
                 return next;
