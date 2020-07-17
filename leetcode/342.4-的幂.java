5eaba0c07ae3b14e038d6b672a1fd500afc383ec
F.LuVx <yeyu0926@hotmail.com>
2020-07-17 14:16:00 +0800

    324

---
+++ leetcode/342.4-的幂.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=342 lang=java
+ *
+ * [342] 4的幂
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        return num > 0
+                && (num & (num - 1)) == 0
+                && (num & 0x55555555) != 0;
+    }
+}
+// @lc code=end
+
