aec68be1a697f7535560f1df61dafe97741be226
F.LuVx <yeyu0926@hotmail.com>
2021-01-29 00:05:37 +0800

    update

--- other/src/main/kotlin/org/luvx/question/跳跃游戏.kt
+++ other/src/main/kotlin/org/luvx/question/跳跃游戏.kt
@@ -1,16 +1,20 @@
+package org.luvx.question
+
 /**
  * @author: Ren, Xie
  */
-public class Main {
-
+class Solution {
     /**
      * 给定一个非负整数数组，你最初位于数组的第一个位置。
      * 首先跳跃一个长度, 为[1, length/2)
-     * <p>
+     *
+     *
      * 之后跳跃长度为此处的元素值
-     * <p>
+     *
+     *
      * 判断能到达最后一个位置时, 最小的跳跃次数, 不能则返回-1
-     * <p>
+     *
+     *
      * 7 5 9 4 2 6 8 3 5 4 3 9 -> 2
      * 1 2 3 7 1 5 9 3 2 1 -> -1
      * 1 2 3 7 5 5 9 3 2 1 -> 2
@@ -18,32 +22,34 @@ public class Main {
      * @param nums
      * @return
      */
-    public int steps(int[] nums) {
-        int result = -1;
-        int len = nums.length, half = len / 2;
-        for (int i = 1; i < half; i++) {
-            int index = i, count = 0;
+    fun steps(nums: IntArray): Int {
+        var result = -1
+        val len = nums.size
+        val half = len / 2
+        for (i in 1 until half) {
+            var index = i
+            var count = 0
             while (true) {
                 if (index <= len - 1) {
-                    count++;
+                    count++
                     if (index == len - 1) {
-                        break;
+                        break
                     }
                 } else if (index > len - 1) {
-                    count = -1;
-                    break;
+                    count = -1
+                    break
                 }
-                int num = nums[index];
-                index += num;
+                val num = nums[index]
+                index += num
             }
             if (result == -1) {
-                result = count;
+                result = count
             } else {
                 if (count != -1) {
-                    result = Math.min(count, result);
+                    result = count.coerceAtMost(result)
                 }
             }
         }
-        return result;
+        return result
     }
 }
---
+++ other/src/test/java/org/luvx/remember/sort/SortTest.java
@@ -0,0 +1,15 @@
+package org.luvx.remember.sort;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class SortTest {
+    int[] array = {5, 4, 3, 2, 1};
+
+    @Test
+    public void test() {
+        Sorts.sort5(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
