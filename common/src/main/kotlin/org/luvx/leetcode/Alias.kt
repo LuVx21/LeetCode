39687d3249dad087fa97ddb4f3246dfb59af0a63
F.LuVx <yeyu0926@hotmail.com>
2021-02-07 22:22:17 +0800

    459

--- common/src/main/kotlin/org/luvx/leetcode/Alias.kt
+++ common/src/main/kotlin/org/luvx/leetcode/Alias.kt
@@ -2,3 +2,13 @@ package org.luvx.leetcode
 
 public typealias ListNodeKt = ListNode
 public typealias TreeKt = TreeNode
+
+fun Any.out(vararg messages: Any?) {
+    println(this)
+    if (messages.isNotEmpty()) {
+        println("-----------------")
+        for (message in messages) {
+            println(message)
+        }
+    }
+}
