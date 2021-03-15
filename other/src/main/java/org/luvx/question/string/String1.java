11089f320c6ea53248b05455bab5b1500f9756b5
F.LuVx <yeyu0926@hotmail.com>
2021-03-15 15:57:31 +0800

    去除字符串中连续重复

---
+++ other/src/main/java/org/luvx/question/string/String1.java
@@ -0,0 +1,36 @@
+package org.luvx.question.string;
+
+import com.google.common.collect.Lists;
+
+public class String1 {
+    /**
+     * 去除字符串中的连续重复, 只保留一个
+     */
+    private String filter(String s) {
+        int len;
+        if (s == null || (len = s.length()) == 0 || len == 1) {
+            return s;
+        }
+        char[] array = s.toCharArray();
+        int start = len - 1;
+        for (int j = start - 1; j >= 0; ) {
+            while (j >= 0 && array[j] == array[start]) {
+                j--;
+            }
+            if (j < 0) {
+                break;
+            }
+            array[--start] = array[j--];
+        }
+        return new String(array, start, s.length() - start);
+    }
+
+    public static void main(String[] args) {
+        final String1 exec = new String1();
+        Lists.newArrayList(
+                "bbcb", "abcdefffg", "aaaaa"
+        ).stream()
+                .map(exec::filter)
+                .forEach(System.out::println);
+    }
+}
