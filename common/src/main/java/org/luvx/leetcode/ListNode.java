543d2779b26ff4497ce01d34d75256d09c782660
F.LuVx <yeyu0926@hotmail.com>
2021-03-19 19:25:21 +0800

    update

--- common/src/main/java/org/luvx/leetcode/ListNode.java
+++ common/src/main/java/org/luvx/leetcode/ListNode.java
@@ -1,12 +1,14 @@
 package org.luvx.leetcode;
 
 import lombok.AllArgsConstructor;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author Ren, Xie
  */
 @Slf4j
+@ToString
 @AllArgsConstructor
 public class ListNode {
     public int      val;
