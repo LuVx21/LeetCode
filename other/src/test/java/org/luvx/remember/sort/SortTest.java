6716b2677080e862295e26e03214cc744420a3ca
F.LuVx <yeyu0926@hotmail.com>
2021-03-19 19:25:07 +0800

    update

--- other/src/test/java/org/luvx/remember/sort/SortTest.java
+++ other/src/test/java/org/luvx/remember/sort/SortTest.java
@@ -1,5 +1,6 @@
 package org.luvx.remember.sort;
 
+import org.junit.After;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -8,8 +9,33 @@ public class SortTest {
     int[] array = {5, 4, 3, 2, 1};
 
     @Test
-    public void test() {
+    public void test0() {
+        BubbleSort.sort0(array);
+    }
+
+    @Test
+    public void test1() {
+        QuickSort.sort(array, 0, array.length - 1);
+    }
+
+    @Test
+    public void test2() {
         SimpleSort.sort5(array);
+    }
+
+    @Test
+    public void test3() {
+        int i = 10;
+        a(i++);
+        System.out.println(i);
+    }
+
+    private void a(int i) {
+        System.out.println(i);
+    }
+
+    @After
+    public void after() {
         System.out.println(Arrays.toString(array));
     }
 }
