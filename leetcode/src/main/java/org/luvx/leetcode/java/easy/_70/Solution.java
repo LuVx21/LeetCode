17c78e852c0d03173be0830c904173dcbab59be9
F.LuVx <yeyu0926@hotmail.com>
2021-03-20 00:04:19 +0800

    update

--- leetcode/src/main/java/org/luvx/leetcode/java/easy/_70/Solution.java
+++ leetcode/src/main/java/org/luvx/leetcode/java/easy/_70/Solution.java
@@ -1,3 +1,5 @@
+package org.luvx.leetcode.java.easy._70;
+
 /*
  * @lc app=leetcode.cn id=70 lang=java
  *
@@ -6,13 +8,18 @@
 
 // @lc code=start
 class Solution {
-
+    /**
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     * <p>
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     */
     public int climbStairs(int n) {
         if (n > 0 && n <= 2) {
             return n;
         }
         int[] mem = new int[n];
-        mem[0] = 1; mem[1] = 2;
+        mem[0] = 1;
+        mem[1] = 2;
         for (int i = 2; i < n; i++) {
             mem[i] = mem[i - 1] + mem[i - 2];
         }
@@ -21,9 +28,8 @@ class Solution {
 
     /*
     private static ConcurrentHashMap<Integer, Integer> cache = new ConcurrentHashMap<>();
-
     /**
-     * 非递归+缓存
+     * 非递归 + 缓存
      *
      * @param n > 0
      * @return
