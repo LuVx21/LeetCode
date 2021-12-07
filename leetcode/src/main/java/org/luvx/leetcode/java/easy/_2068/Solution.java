106d7889a560bb4d86d6aed4dd2510b9cc0ac5d8
F.LuVx <yeyu0926@hotmail.com>
2021-12-07 22:30:30 +0800

    2068 update

--- leetcode/src/main/java/org/luvx/leetcode/java/easy/_2068/Solution.java
+++ leetcode/src/main/java/org/luvx/leetcode/java/easy/_2068/Solution.java
@@ -9,13 +9,9 @@ package org.luvx.leetcode.java.easy._2068;
 class Solution {
     public boolean checkAlmostEquivalent(String word1, String word2) {
         int[] array = new int[26];
-        for (char c : word1.toCharArray()) {
-            int i = c - 'a';
-            array[i]++;
-        }
-        for (char c : word2.toCharArray()) {
-            int i = c - 'a';
-            array[i]--;
+        for (int i = 0; i < word1.length(); i++) {
+            array[word1.charAt(i) - 'a']++;
+            array[word2.charAt(i) - 'a']--;
         }
         for (int i : array) {
             if (i > 3 || i < -3) {
