9a6dc91d3944caf886d51f2bf07895211b41f35f
F.LuVx <yeyu0926@hotmail.com>
2021-12-08 21:17:23 +0800

    1027

---
+++ leetcode/src/main/kotlin/org/luvx/leetcode/medium/_1027/1027.最长等差数列.kt
@@ -0,0 +1,40 @@
+package org.luvx.leetcode.medium._1027
+
+import org.luvx.leetcode.out
+
+/*
+ * @lc app=leetcode.cn id=1027 lang=kotlin
+ *
+ * [1027] 最长等差数列
+ */
+
+// @lc code=start
+class Solution {
+    /**
+     * 给定一个整数数组 A，返回 A 中最长等差子序列的长度
+     * 3,6,9,12 -> 4
+     * 3,3,6,9,12 -> 4
+     * 9,4,7,2,10 -> 4,7,10 -> 3
+     * 20,1,15,3,10,5,8 -> 20,15,10,5 -> 4
+     */
+    fun longestArithSeqLength(nums: IntArray): Int {
+        var cnt = 2
+        val mapArray = Array(nums.size) { HashMap<Int, Int>() }
+        for (i in nums.indices) {
+            val map = mapArray[i]
+            for (j in 0 until i) {
+                val d = nums[i] - nums[j]
+                val value = mapArray[j].getOrDefault(d, 1) + 1
+                map[d] = value
+                cnt = cnt.coerceAtLeast(value)
+            }
+        }
+        return cnt
+    }
+}
+// @lc code=end
+
+fun main() {
+    val exec = Solution()
+    exec.longestArithSeqLength(intArrayOf(9, 4, 7, 2, 10)).out()
+}
---
+++ other/src/main/kotlin/org/luvx/question/最长连续等差数列.kt
@@ -0,0 +1,33 @@
+package org.luvx.question
+
+import org.luvx.leetcode.out
+
+/**leetcode-1027 扩展
+ * 给定一个整数数组 A，返回 A 中最长连续等差子序列的长度
+ */
+fun longestArithSeqLength(nums: IntArray): Int {
+    var value = nums[1] - nums[0]
+    var cnt = 1
+    var temp = cnt
+    for (i in 2 until nums.size) {
+        val v = nums[i] - nums[i - 1]
+        if (v == value) {
+            temp++
+        } else {
+            temp = 1
+            value = v
+        }
+        cnt = cnt.coerceAtLeast(temp)
+    }
+    return cnt + 1
+}
+
+fun main() {
+    listOf(
+        intArrayOf(0, 5, 14, 19, 24, 29),
+        intArrayOf(1, 2, 4, 7),
+        intArrayOf(3, 6, 7, 8),
+        intArrayOf(3, 6, 9, 12)
+    )
+        .forEach { array -> longestArithSeqLength(array).out() }
+}
