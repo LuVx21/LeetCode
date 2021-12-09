290624aa9d6edef433cf75e01f4e96a02fbae3a3
F.LuVx <yeyu0926@hotmail.com>
2021-12-09 15:45:44 +0800

    263

---
+++ leetcode/src/main/kotlin/org/luvx/leetcode/easy/_263/263.丑数.kt
@@ -0,0 +1,39 @@
+package org.luvx.leetcode.easy._263
+
+import org.luvx.leetcode.out
+
+// @lc code=start
+/*
+ * @lc app=leetcode.cn id=263 lang=kotlin
+ *
+ * [263] 丑数
+ */
+class Solution {
+    /**
+     * 给你一个整数 n ，请你判断 n 是否为 丑数 。如果是，返回 true ；否则，返回 false 。
+     * 丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+     * 14 = 2*7 false
+     * 30 = 2*3*5 true
+     * <=0 false
+     */
+    fun isUgly(n: Int): Boolean {
+        if (n <= 1) {
+            return n == 1
+        }
+        var temp = n
+        val intArrayOf = intArrayOf(2, 3, 5)
+        for (i in intArrayOf) {
+            while (temp % i == 0) {
+                temp /= i
+            }
+        }
+        return temp == 1
+    }
+}
+// @lc code=end
+
+fun main() {
+    val exec = Solution()
+    intArrayOf(0, 1, 14, 30)
+        .forEach { n -> exec.isUgly(n).out() }
+}
