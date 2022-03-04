3d0ef4d73a23084c169af6f2f7d674ee9cc5293c
F.LuVx <yeyu0926@hotmail.com>
2022-03-04 15:31:14 +0800

    update

--- common/src/test/java/org/luvx/leetcode/UtilsTest.java
+++ common/src/test/java/org/luvx/leetcode/UtilsTest.java
@@ -1,7 +1,8 @@
 package org.luvx.leetcode;
 
+import org.junit.jupiter.api.Test;
+
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
 
 @Slf4j
 public class UtilsTest {
---
+++ other/src/main/java/org/luvx/question/string/Q2.java
@@ -0,0 +1,60 @@
+package org.luvx.question.string;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Q2 {
+    /**
+     * 合法括号序列判断
+     * 给定一个字符串A，请返回一个bool值代表它是否为一个合法的括号串（只能由括号组成）。
+     * <pre>
+     *     (()()) -> true
+     *     ()a()() -> false
+     *     ()(()() -> ( -> false
+     *     ())()()(((d -> )(((d -> false
+     * </pre>
+     */
+    public boolean chkParenthesis(String s) {
+        int cnt = 0;
+        for (char c : s.toCharArray()) {
+            if (c != '(' && c != ')') {
+                return false;
+            }
+            cnt += (c == '(' ? 1 : -1);
+        }
+        return cnt == 0;
+    }
+
+    public boolean chkParenthesis1(String s) {
+        if (s == null || s.length() == 0) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c != '(' && c != ')') {
+                return false;
+            }
+            if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.empty();
+    }
+
+    /**
+     * Map<()>
+     * <)(>
+     */
+    public static void main(String[] args) {
+        final Q2 exec = new Q2();
+        List.of("(()())",
+                "()a()()",
+                "()(()()",
+                "())()()(((d",
+                "))(())"
+        ).forEach(s -> System.out.println(exec.chkParenthesis(s)));
+    }
+}
