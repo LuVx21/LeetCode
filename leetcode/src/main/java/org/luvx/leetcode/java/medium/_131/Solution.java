40df41e6c9f9a997c413bf476999016f3afb8b73
F.LuVx <yeyu0926@hotmail.com>
2022-03-10 18:40:01 +0800

    131

---
+++ leetcode/src/main/java/org/luvx/leetcode/java/medium/_131/Solution.java
@@ -0,0 +1,44 @@
+package org.luvx.leetcode.java.medium._131;/*
+ * @lc app=leetcode.cn id=131 lang=java
+ *
+ * [131] 分割回文串
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), s, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<String>> result, List<String> list, String s, int start) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = start; i < s.length(); i++) {
+            if (!isPalindrome(s, start, i)) {
+                continue;
+            }
+            list.add(s.substring(start, i + 1));
+            backtrack(result, list, s, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    private boolean isPalindrome(String s, int low, int high) {
+        while (low < high) {
+            if (s.charAt(low++) != s.charAt(high--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
+
