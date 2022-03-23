ef94080f78d9e6493e823f3b7caf4b1bbe7eef09
F.LuVx <yeyu0926@hotmail.com>
2022-03-23 20:30:46 +0800

    300 491

---
+++ leetcode/src/main/java/org/luvx/leetcode/java/medium/_300/Solution.java
@@ -0,0 +1,91 @@
+package org.luvx.leetcode.java.medium._300;
+/*
+ * @lc app=leetcode.cn id=300 lang=java
+ *
+ * [300] 最长递增子序列
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// @lc code=start
+class Solution {
+    int result = 1;
+
+    public int lengthOfLIS(int[] nums) {
+        int[] tails = new int[nums.length];
+        int max = 0;
+        for (int num : nums) {
+            int left = 0, right = max;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (tails[mid] < num) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            tails[left] = num;
+            if (left == max) {
+                max++;
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 动态规划
+     */
+    public int lengthOfLIS0(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        int res = 0;
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    /**
+     * 超时: 性能问题
+     */
+    public int lengthOfLIS1(int[] nums) {
+        helper(new ArrayList<>(), 0, nums);
+        return result;
+    }
+
+    private void helper(List<Integer> list, int index, int[] nums) {
+        if (list.size() >= 2 && list.size() > result) {
+            result = list.size();
+        }
+        for (int i = index; i < nums.length; i++) {
+            if (list.size() == 0 || list.get(list.size() - 1) < nums[i]) {
+                list.add(nums[i]);
+                helper(list, i + 1, nums);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution exec = new Solution();
+        List.of(
+                new int[] {7, 7, 7, 7, 7, 7, 7},
+                new int[] {0, 1, 0, 3, 2, 3},
+                new int[] {10, 9, 2, 5, 3, 7, 101, 18}
+        ).forEach(array ->
+                System.out.println(exec.lengthOfLIS(array))
+        );
+    }
+}
+// @lc code=end
+
---
+++ leetcode/src/main/java/org/luvx/leetcode/java/medium/_491/Solution.java
@@ -0,0 +1,71 @@
+package org.luvx.leetcode.java.medium._491;
+/*
+ * @lc app=leetcode.cn id=491 lang=java
+ *
+ * [491] 递增子序列
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+    /**
+     * 回溯法
+     * 4,6,7,7
+     */
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        helper(result, new ArrayList<>(), 0, nums);
+        return result;
+    }
+
+    private void helper(List<List<Integer>> result, List<Integer> list, int index, int[] nums) {
+        if (list.size() >= 2) {
+            result.add(new ArrayList<>(list));
+        }
+        Set<Integer> used = new HashSet<>();
+        for (int i = index; i < nums.length; i++) {
+            if (used.contains(nums[i])) {
+                continue;
+            }
+            if (list.size() == 0 || list.get(list.size() - 1) <= nums[i]) {
+                used.add(nums[i]);
+                list.add(nums[i]);
+                helper(result, list, i + 1, nums);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+
+    public List<List<Integer>> findSubsequences1(int[] nums) {
+        Set<List<Integer>> res = new HashSet<>();
+        findSequence(res, new ArrayList<>(), 0, nums);
+        return new ArrayList<>(res);
+    }
+
+    public void findSequence(Set<List<Integer>> result, List<Integer> list, int index, int[] nums) {
+        if (list.size() >= 2) {
+            result.add(new ArrayList<>(list));
+        }
+        for (int i = index; i < nums.length; i++) {
+            if (list.size() == 0 || list.get(list.size() - 1) <= nums[i]) {
+                list.add(nums[i]);
+                findSequence(result, list, i + 1, nums);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution exec = new Solution();
+        int[] array = {4, 6, 7, 7};
+        List<List<Integer>> subsequences = exec.findSubsequences(array);
+        System.out.println(subsequences);
+    }
+}
+// @lc code=end
