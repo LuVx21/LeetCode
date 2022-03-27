a04434116be2e4a4a70fd9f7a3b16e7f258fe878
F.LuVx <yeyu0926@hotmail.com>
2022-03-28 00:17:32 +0800

    64

---
+++ leetcode/src/main/java/org/luvx/leetcode/java/medium/_64/Solution.java
@@ -0,0 +1,58 @@
+package org.luvx.leetcode.java.medium._64;
+/*
+ * @lc app=leetcode.cn id=64 lang=java
+ *
+ * [64] 最小路径和
+ */
+
+// @lc code=start
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int row = grid.length, col = grid[0].length;
+        int[][] dp = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                // 上方的
+                int i1 = i == 0 ? 0 : dp[i - 1][j];
+                // 左侧的
+                int i2 = j == 0 ? 0 : dp[i][j - 1];
+                int min = 0;
+                if (i != 0 && j != 0) {
+                    min = Math.min(i1, i2);
+                } else {
+                    min = i1 + i2;
+                }
+                dp[i][j] = min + grid[i][j];
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+
+    public int minPathSum2(int[][] grid) {
+        int row = grid.length, col = grid[0].length;
+        return min(grid, row - 1, col - 1);
+    }
+
+    private int min(int[][] grid, int row, int col) {
+        if (row == 0 && col == 0) {
+            return grid[row][col];
+        }
+        if (row == 0) {
+            return grid[row][col] + min(grid, row, col - 1);
+        }
+        if (col == 0) {
+            return grid[row][col] + min(grid, row - 1, col);
+        }
+        return grid[row][col] + Math.min(min(grid, row - 1, col), min(grid, row, col - 1));
+    }
+
+    public static void main(String[] args) {
+        Solution exec = new Solution();
+        // int[][] array = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        int[][] array = new int[][] {{1, 2, 3}, {4, 5, 6}};
+        System.out.println(exec.minPathSum(array));
+    }
+}
+// @lc code=end
+
