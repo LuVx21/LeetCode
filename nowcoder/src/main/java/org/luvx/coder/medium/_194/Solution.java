71fb38cc49e998e6d7cd1afd7a776ec61e52cf5a
F.LuVx <yeyu0926@hotmail.com>
2022-03-27 19:42:26 +0800

    update

---
+++ nowcoder/src/main/java/org/luvx/coder/medium/_194/Solution.java
@@ -0,0 +1,12 @@
+package org.luvx.coder.medium._194;
+
+public class Solution {
+    /**
+     * 同leetcode-31, 已完成
+     *
+     * @param nums int整型一维数组
+     */
+    public int[] nextPermutation(int[] nums) {
+        return null;
+    }
+}
