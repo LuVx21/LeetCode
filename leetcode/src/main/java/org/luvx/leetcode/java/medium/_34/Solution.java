4a9f2f04553a4a83bec19cfcc133e040259d3455
F.LuVx <yeyu0926@hotmail.com>
2022-05-28 00:41:34 +0800

    34

---
+++ leetcode/src/main/java/org/luvx/leetcode/java/medium/_34/Solution.java
@@ -0,0 +1,60 @@
+package org.luvx.leetcode.java.medium._34;
+/*
+ * @lc app=leetcode.cn id=34 lang=java
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ */
+
+// @lc code=start
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+class Solution {
+    /**
+     * 5,7,7,8,8,10 - 8 -> [3,4]
+     * 5,7,7,8,8,9,10 - 9 -> [5,5]
+     */
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = {-1, -1};
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] >= target) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        if (nums[start] != target) {
+            return result;
+        }
+        result[0] = start;
+
+        end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2 + 1;
+            if (nums[mid] <= target) {
+                start = mid;
+            } else {
+                end = mid - 1;
+            }
+        }
+        result[1] = end;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 7, 7, 8, 8, 9, 10};
+        Solution exec = new Solution();
+        Stream.of(8, 9, 11)
+                .map(t -> exec.searchRange(array, t))
+                .map(Arrays::toString)
+                .forEach(System.out::println);
+    }
+}
+// @lc code=end
+
