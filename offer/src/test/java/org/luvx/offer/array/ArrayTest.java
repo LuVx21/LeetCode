8a35ca21c194b68be1a621db64be5e51cd48d198
F.LuVx <yeyu0926@hotmail.com>
2022-06-08 20:49:10 +0800

    update

--- offer/src/test/java/org/luvx/offer/array/ArrayTest.java
+++ offer/src/test/java/org/luvx/offer/array/ArrayTest.java
@@ -1,11 +1,8 @@
 package org.luvx.offer.array;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
-import org.luvx.common.RunInReflect;
-
-import com.alibaba.fastjson.JSON;
 
 class ArrayTest {
 
@@ -16,17 +13,13 @@ class ArrayTest {
         int[] numbers3 = {2, 4, 2, 1, 4};
         int[] numbers4 = {2, 1, 3, 0, 4};
         int[] numbers5 = {2, 1, 3, 5, 4};
-        List<Object> duplicate = RunInReflect.exec(Offer51.class, "duplicate",
-                numbers1,
-                numbers2,
-                numbers3,
-                numbers4,
-                numbers5
-        );
 
-        duplicate.stream()
-                .map(JSON::toJSONString)
-                .map(s -> 51 + ": " + s)
+        Offer51 exec = new Offer51();
+        Stream.of(
+                        numbers1, numbers2, numbers3,
+                        numbers4, numbers5
+                )
+                .map(exec::duplicate)
                 .forEachOrdered(System.out::println);
     }
 }
